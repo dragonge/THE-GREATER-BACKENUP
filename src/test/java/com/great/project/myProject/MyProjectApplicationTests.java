61bdbdd8428d4ab4e2438e16cb53824ad751b6f1
dragonge <346331488@qq.com>
2020-03-13 21:32:58 +0800

    [add]: 项目初始化

---
+++ src/test/java/com/great/project/myProject/MyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.great.project.myProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
